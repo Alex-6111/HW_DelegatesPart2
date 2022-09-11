600a821036997b9c6bff43080a54205d56333889
Admin <obekas777@gmail.com>
2022-09-12 00:22:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HW_DelegatesPart2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           
+        }
+
+        public delegate string ConvertColor(System.Drawing.Color color);
+        public class Dispacher
+        {
+            public event ConvertColor colorEvent;
+            public string ConvertColor(System.Drawing.Color color)
+            {
+                if (colorEvent != null) return colorEvent(color);
+                throw new NullReferenceException();
+            }
+        }
+
+        public int MultipleOfSeven(int[] arr)
+        {
+            return arr.Count(n => n % 7 == 0);
+        }
+        public int PositiveInArr(int[] arr)
+        {
+            return arr.Count(n => n > 0);
+        }
+        public int NegativeInArr(int[] arr)
+        {
+            return arr.Count(n => n < 0);
+        }
+        public bool FindWord(string[] text, string word)
+        {
+            if (text.Where(x => x.StartsWith(word)).FirstOrDefault() == word) return true;
+            else return false;
+        }
+    }
+}
