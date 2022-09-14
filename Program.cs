3dfc95055ac59817a7c9f5dfec2d90c1df1d7152
Admin <obekas777@gmail.com>
2022-09-14 22:52:20 +0300

    <>

--- Program.cs
+++ Program.cs
@@ -4,36 +4,8 @@
     {
         static void Main()
         {
-           
+            //initial
         }
 
-        public delegate string ConvertColor(System.Drawing.Color color);
-        public class Dispacher
-        {
-            public event ConvertColor colorEvent;
-            public string ConvertColor(System.Drawing.Color color)
-            {
-                if (colorEvent != null) return colorEvent(color);
-                throw new NullReferenceException();
-            }
-        }
-
-        public int MultipleOfSeven(int[] arr)
-        {
-            return arr.Count(n => n % 7 == 0);
-        }
-        public int PositiveInArr(int[] arr)
-        {
-            return arr.Count(n => n > 0);
-        }
-        public int NegativeInArr(int[] arr)
-        {
-            return arr.Count(n => n < 0);
-        }
-        public bool FindWord(string[] text, string word)
-        {
-            if (text.Where(x => x.StartsWith(word)).FirstOrDefault() == word) return true;
-            else return false;
-        }
-    }
+    }  
 }
